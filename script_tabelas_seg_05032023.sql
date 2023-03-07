aab947276d14a48a62592643240194ec13ba6f2b
David Kestering <davidkestering@gmail.com>
2023-03-07 13:41:33 -0300

    atualizacao para log mais completo
    atualizacao para modelo de log de transacao mais completo

--- script_tabelas_seg_05032023.sql
+++ script_tabelas_seg_05032023.sql
@@ -5,6 +5,7 @@ CREATE TABLE seg_categoria_tipo_transacao (
   dt_cadastro	DATETIME DEFAULT GETDATE() NOT NULL,
   publicado		TINYINT CHECK(publicado >= 0) NOT NULL DEFAULT 0,
   ativo			TINYINT  CHECK(ativo >= 0) NOT NULL DEFAULT 1
+  CONSTRAINT seg_categoria_tipo_transacao_descricao UNIQUE(descricao)
 );
 
 INSERT INTO seg_categoria_tipo_transacao (descricao) VALUES
@@ -29,34 +30,39 @@ CREATE TABLE seg_erros_mysql (
   ativo			TINYINT  CHECK(ativo >= 0) NOT NULL DEFAULT 1
 );
 
+drop table seg_grupo_usuario;
 CREATE TABLE seg_grupo_usuario (
   id				INT IDENTITY(1,1) PRIMARY KEY,
-  nm_grupo_usuario	NVARCHAR(MAX) NOT NULL,
+  nm_grupo_usuario	VARCHAR(255) NOT NULL,
   dt_cadastro		DATETIME DEFAULT GETDATE() NOT NULL,
   publicado		TINYINT CHECK(publicado >= 0) NOT NULL DEFAULT 0,
-  ativo			TINYINT  CHECK(ativo >= 0) NOT NULL DEFAULT 1
+  ativo			TINYINT  CHECK(ativo >= 0) NOT NULL DEFAULT 1,
+  CONSTRAINT seg_grupo_usuario_nm_grupo_usuario UNIQUE(nm_grupo_usuario)
 );
 
 INSERT INTO seg_grupo_usuario (nm_grupo_usuario) VALUES
 ('Administrador');
 
 
+--drop table seg_permissao
 CREATE TABLE seg_permissao (
   id_tipo_transacao INT CHECK(id_tipo_transacao >= 0) NOT NULL DEFAULT 0,
   id_grupo_usuario	INT CHECK(id_grupo_usuario >= 0) NOT NULL DEFAULT 0,
   dt_cadastro		DATETIME DEFAULT GETDATE() NOT NULL,
   publicado		TINYINT CHECK(publicado >= 0) NOT NULL DEFAULT 0,
-  ativo			TINYINT  CHECK(ativo >= 0) NOT NULL DEFAULT 1
+  ativo			TINYINT  CHECK(ativo >= 0) NOT NULL DEFAULT 1,
+  PRIMARY KEY (id_tipo_transacao, id_grupo_usuario)
 );
 
-
+drop table seg_tipo_transacao
 CREATE TABLE seg_tipo_transacao (
   id							INT IDENTITY(1,1) PRIMARY KEY,
   id_categoria_tipo_transacao	INT CHECK(id_categoria_tipo_transacao >= 0) NOT NULL DEFAULT 0,
-  transacao						NVARCHAR(MAX) NOT NULL,
+  transacao						VARCHAR(255) NOT NULL,
   dt_cadastro		DATETIME DEFAULT GETDATE() NOT NULL,
   publicado		TINYINT CHECK(publicado >= 0) NOT NULL DEFAULT 0,
-  ativo			TINYINT  CHECK(ativo >= 0) NOT NULL DEFAULT 1
+  ativo			TINYINT  CHECK(ativo >= 0) NOT NULL DEFAULT 1,
+  CONSTRAINT seg_tipo_transacao_categoria_tipo_transacao UNIQUE(id_categoria_tipo_transacao,transacao)
 );
 
 
@@ -112,23 +118,30 @@ INSERT INTO seg_permissao (id_tipo_transacao, id_grupo_usuario) VALUES
 (22, 1);
 
 
+drop table seg_transacao;
 CREATE TABLE seg_transacao (
   id				INT IDENTITY(1,1) PRIMARY KEY,
   id_tipo_transacao INT CHECK(id_tipo_transacao >= 0) NOT NULL DEFAULT 0,
   id_usuario		INT CHECK(id_usuario >= 0) NOT NULL DEFAULT 0,
   objeto			NVARCHAR(MAX) NOT NULL,
+  campo				NVARCHAR(MAX) NOT NULL,
+  valor_antigo		NVARCHAR(MAX) NOT NULL,
+  valor_novo		NVARCHAR(MAX) NOT NULL,
   ip				NVARCHAR(MAX) DEFAULT NULL,
   dt_cadastro		DATETIME DEFAULT GETDATE() NOT NULL,
   publicado		TINYINT CHECK(publicado >= 0) NOT NULL DEFAULT 0,
   ativo			TINYINT  CHECK(ativo >= 0) NOT NULL DEFAULT 1
 );
 
-
+drop table seg_transacao_acesso;
 CREATE TABLE seg_transacao_acesso (
   id				INT IDENTITY(1,1) PRIMARY KEY,
   id_tipo_transacao INT CHECK(id_tipo_transacao >= 0) NOT NULL DEFAULT 0,
   id_usuario		INT CHECK(id_usuario >= 0) NOT NULL DEFAULT 0,
   objeto			NVARCHAR(MAX) NOT NULL,
+  campo				NVARCHAR(MAX) NOT NULL,
+  valor_antigo		NVARCHAR(MAX) NOT NULL,
+  valor_novo		NVARCHAR(MAX) NOT NULL,
   ip				NVARCHAR(MAX) DEFAULT NULL,
   dt_cadastro		DATETIME DEFAULT GETDATE() NOT NULL,
   publicado		TINYINT CHECK(publicado >= 0) NOT NULL DEFAULT 0,
@@ -140,21 +153,27 @@ CREATE TABLE seg_usuario (
   id				INT IDENTITY(1,1) PRIMARY KEY,
   id_grupo_usuario	INT CHECK(id_grupo_usuario >= 0) NOT NULL DEFAULT 0,
   nm_usuario		NVARCHAR(MAX) NOT NULL,
-  login				NVARCHAR(MAX) NOT NULL,
+  login				VARCHAR(255) NOT NULL,
   senha				VARBINARY(MAX) NOT NULL,
   email				NVARCHAR(MAX) DEFAULT NULL,
   logado			TINYINT CHECK(logado >= 0) NOT NULL DEFAULT 0,
   dt_cadastro		DATETIME DEFAULT GETDATE() NOT NULL,
   publicado		TINYINT CHECK(publicado >= 0) NOT NULL DEFAULT 0,
-  ativo			TINYINT  CHECK(ativo >= 0) NOT NULL DEFAULT 1
+  ativo			TINYINT  CHECK(ativo >= 0) NOT NULL DEFAULT 1,
+  CONSTRAINT seg_usuario_login UNIQUE(login)
 );
 
 DECLARE @senha NVARCHAR(MAX) = 'tudobem';
 DECLARE @senhaCriptografada VARBINARY(MAX);
 SET @senhaCriptografada = HASHBYTES('SHA2_256', @senha);
-
 INSERT INTO seg_usuario (id_grupo_usuario, nm_usuario, login, senha, email) VALUES
 (1, 'Dávìd Këstêrîng', 'david', @senhaCriptografada, 'davidkestering@gmail.com');
+
+DECLARE @senha NVARCHAR(MAX) = 'tudobem';
+DECLARE @senhaCriptografada VARBINARY(MAX);
+SET @senhaCriptografada = HASHBYTES('SHA2_256', @senha);
+INSERT INTO seg_usuario (id_grupo_usuario, nm_usuario, login, senha, email) VALUES
+(1, 'teste', 'teste', @senhaCriptografada, 'teste@gmail.com');
 
 select * from seg_usuario;
 
